df73674b36568930e1dbc2aa724e999cd13ea201
AbuBakar <muhammadabubakar19187@gmail.com>
2025-09-03 11:09:17 +0500

    Home Page layout complete, some final touch will be done later (clock, animations etc)

--- k72/src/components/home/HomeHeroText.jsx
+++ k72/src/components/home/HomeHeroText.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div>
-      <div>L'étincelle</div>
-      <div>quigénère</div>
-      <div>la créativité</div>
+    <div className='font-[font1] text-white text-center'>
+      <div className='text-[9.3vw] uppercase leading-[10vw] flex items-center justify-center'>L'étincelle</div>
+      <div className='text-[9.3vw] uppercase leading-[8vw] flex items-center justify-center'>qui<div className='h-[8vw] w-[16vw] rounded-full overflow-hidden -mt-3'><Video/></div>génère</div>
+      <div className='text-[9.3vw] uppercase leading-[9vw] flex items-center justify-center'>la créativité</div>
     </div>
   )
 }
 
 export default HomeHeroText
+ 
